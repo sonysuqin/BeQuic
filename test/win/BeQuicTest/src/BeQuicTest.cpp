d035be73c4ab21ef1b0731357ffd481451133298
sonysuqin <sonysuqin@gmail.com>
2019-04-26 11:31:50 +0800

    Adapt Windows test program to new api.

--- test/win/BeQuicTest/src/BeQuicTest.cpp
+++ test/win/BeQuicTest/src/BeQuicTest.cpp
@@ -2,23 +2,62 @@
 #include "be_quic.h"
 #include <string>
 
+#include <windows.h>
+
+typedef uint64_t TimeType;
+
+static TimeType get_tickcount() {
+#ifdef WIN32
+    //return ::GetTickCount();
+    LARGE_INTEGER freq, counter;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&counter);
+    return (1000000L * counter.QuadPart / freq.QuadPart) / 1000L;
+#elif defined ANDROID
+    timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * (TimeType)1000 + ts.tv_nsec / 1000000;
+#elif defined __IOS__
+    mach_timebase_info_data_t mach_info;
+    mach_timebase_info(&mach_info);
+    double factor = static_cast<double>(mach_info.numer) / mach_info.denom;
+    return (mach_absolute_time() * factor) / 1000000L;
+#elif defined _LINUX_
+    timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * (TimeType)1000 + ts.tv_nsec / 1000000;
+#else 
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return tv.tv_sec * (TimeType)1000 + tv.tv_usec / 1000;
+#endif
+}
+
 bool g_write_file   = true;
 FILE *g_fp          = NULL;
 std::string g_data;
 
 int main(int argc, char* argv[]) {
     while (1) {
+        TimeType t1 = GetTickCount();
         int handle = be_quic_open(
             "https://www.example.org:6121",
+            NULL,
+            0,
             "GET",
             NULL,
             0,
             NULL,
             0,
             1,
+            -1,
+            kBeQuic_Handshake_Protocol_Quic_Crypto,
+            43,
             5000);
 
-        printf("Open quic session %d.\n", handle);
+        TimeType t2 = GetTickCount();
+
+        printf("Open quic session %d, spent %I64u ms.\n", handle, t2 - t1);
 
         if (handle <= 0) {
             break;
@@ -44,8 +83,7 @@ int main(int argc, char* argv[]) {
 
             if (g_write_file) {
                 fwrite(buf, read_len, 1, g_fp);
-            }
-            else {
+            } else {
                 g_data.append(std::string((char*)buf, read_len));
             }
             total_len += read_len;
